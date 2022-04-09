e4cfdd50db1bb9077513d3f941dedddaa8f020e9
Amit Nahmias <nahmias.amit@gmail.com>
2022-04-09 12:17:52 +0300

    First final version maman13

--- Maman13/src/Airport.java
+++ Maman13/src/Airport.java
@@ -28,24 +28,25 @@ public class Airport {
         for (int flightNumber = 0; flightNumber < MAX_FLIGHTS; flightNumber++)
             if (_flightsSchedule[flightNumber].equals(f)) {
                 _flightsSchedule[flightNumber] = new Flight(_flightsSchedule[_noOfFlights - 1]);
-                _flightsSchedule[flightNumber] = null;
+                _flightsSchedule[_noOfFlights - 1] = null;
+                _noOfFlights--;
                 return true;
             }
         return false;
     }
 
     public Time1 firstFlightFromOrigin(String place) {
-        Time1 minTime = new Time1(23, 59);
+        Time1 firstDeparture = new Time1(23, 59);
         boolean isExists = false;
         for (int flightNumber = 0; flightNumber < _noOfFlights; flightNumber++) {
             if (_flightsSchedule[flightNumber].getOrigin().equals(place)) {
                 isExists = true;
-                if (minTime.before(_flightsSchedule[flightNumber].getDeparture()))
-                    minTime = new Time1(_flightsSchedule[flightNumber].getDeparture());
+                if (firstDeparture.after(_flightsSchedule[flightNumber].getDeparture()))
+                    firstDeparture = new Time1(_flightsSchedule[flightNumber].getDeparture());
             }
         }
         if (isExists)
-            return minTime;
+            return firstDeparture;
         return null;
     }
 
@@ -69,25 +70,64 @@ public class Airport {
         return counter;
     }
 
-    public String mostPopularDestination(String place) {
+    public String mostPopularDestination() {
         int max = 0;
-        String mostPopularDestination;
-        if (Arrays.stream(_flightsSchedule).)
-            for (int flightNumber = 0; flightNumber < _noOfFlights; flightNumber++) {
-                String destination = new String(_flightsSchedule[flightNumber].getDestination());
-                mostPopularDestination = new String(destination);
-                int counter = 0;
-                for (int flight = flightNumber + 1; flight < _noOfFlights; flight++) {
-                    if (_flightsSchedule[flight].getDestination().equals(place))
-                        counter++;
-                }
-                if (max < counter) {
-                    max = counter;
-                    mostPopularDestination = new String(destination);
-                }
-            }
-        if (max == 0)
+        String mostPopularDestination = null;
+        if (_noOfFlights == 0)
             return null;
+        for (int flightNumber = 0; flightNumber < _noOfFlights; flightNumber++) {
+            String destination = new String(_flightsSchedule[flightNumber].getDestination());
+            int counter = 1; // starts at one because we skip this flight in the next for
+            for (int flight = flightNumber + 1; flight < _noOfFlights; flight++) {
+                if (_flightsSchedule[flight].getDestination().equals(destination))
+                    counter++;
+            }
+            if (max < counter) {
+                max = counter;
+                mostPopularDestination = new String(destination);
+            }
+        }
         return mostPopularDestination;
+    }
+
+    public Flight mostExpensiveTicket() {
+        int maxPrice = 0;
+        Flight mostExpensiveFlight = null;
+        if (_noOfFlights == 0)
+            return null;
+        for (int flightNumber = 0; flightNumber < _noOfFlights; flightNumber++) {
+            int ticketPrice = _flightsSchedule[flightNumber].getPrice();
+            if (ticketPrice > maxPrice) {
+                mostExpensiveFlight = new Flight(_flightsSchedule[flightNumber]);
+                maxPrice = mostExpensiveFlight.getPrice();
+            }
+        }
+        return mostExpensiveFlight;
+    }
+
+    public Flight longestFlight() {
+        int maxDuration = 0;
+        Flight longestFlight = null;
+        if (_noOfFlights == 0)
+            return null;
+        for (int flightNumber = 0; flightNumber < _noOfFlights; flightNumber++) {
+            int duration = _flightsSchedule[flightNumber].getFlightDuration();
+            if (duration > maxDuration) {
+                longestFlight = new Flight(_flightsSchedule[flightNumber]);
+                maxDuration = longestFlight.getFlightDuration();
+            }
+        }
+        return longestFlight;
+    }
+
+    public String toString() {
+        if (_noOfFlights == 0)
+            return null;
+        String output = "The flights for airport " + _city + " today are:";
+        for (int flightNumber = 0; flightNumber < _noOfFlights; flightNumber++) {
+            output = output + "\n";
+            output = output + _flightsSchedule[flightNumber];
+        }
+        return output;
     }
 }
