d291686357440e1a231893522161a48f9c945f63
Amit Nahmias <nahmias.amit@gmail.com>
2022-05-24 18:42:16 +0300

    Fix docs and remove unnecessary lines

--- Maman14/src/Ex14.java
+++ Maman14/src/Ex14.java
@@ -10,7 +10,8 @@
  * @author Amit Nahmias
  * @version 21.5.22
  */
-public class Ex14 {
+public class Ex14
+{
 
     /**
      * Get sorted two dimensions array according to what function and return if value inside him in O(n) run time.
@@ -29,16 +30,17 @@ public class Ex14 {
      * @param val Value to search inside m.
      * @return True if val inside m, otherwise false.
      */
-    public static boolean findValWhat(int[][] m, int val) {
+    public static boolean findValWhat(int[][] m, int val)
+    {
         int row = m.length - 1, column = 0;
-        while (column < m.length && row >= 0) {
+        while (column < m.length && row >= 0)
+        {
             int temp = m[row][column];
             if (temp == val) return true;
             else if (temp > val) row--;
             else column++;
         }
         return false;
-
     }
 
     /**
@@ -60,13 +62,18 @@ public class Ex14 {
      * @param val Value to search inside m.
      * @return True if val inside m, otherwise false.
      */
-    public static boolean findValTest(int[][] m, int val) {
+    public static boolean findValTest(int[][] m, int val)
+    {
         int low = 0, high = m.length - 1, mid;
-        while (low < high - 1) {
+        while (low < high - 1)
+        {
             mid = (low + high) / 2;
-            if (m[mid][0] == val) return true;
-            else if (m[mid][0] > val) high = mid;
-            else low = mid;
+            if (m[mid][0] == val)
+                return true;
+            else if (m[mid][0] > val)
+                high = mid;
+            else
+                low = mid;
         }
         for (int i = 0; i < m.length; i++)
             if (val == m[low][i] || val == m[high][i]) return true;
@@ -87,13 +94,17 @@ public class Ex14 {
      * @param a Input array.
      * @return The number of sorted sub-arrays.
      */
-    public static int strictlyIncreasing(int[] a) {
+    public static int strictlyIncreasing(int[] a)
+    {
         int sum = 0, counter = 1;
-        for (int i = 0; i < a.length - 1; i++) {
-            if (a[i] < a[i + 1]) {
+        for (int i = 0; i < a.length - 1; i++)
+        {
+            if (a[i] < a[i + 1])
+            {
                 counter++;
                 sum = sum + counter - 1;
-            } else counter = 1;
+            }
+            else counter = 1;
         }
         return sum;
     }
@@ -104,8 +115,10 @@ public class Ex14 {
      * @param arr Input array - full in integers.
      * @return The length of the longest flat sub-sequence in array.
      */
-    public static int longestFlatSequence(int[] arr) {
-        if (arr.length <= 1) return arr.length;
+    public static int longestFlatSequence(int[] arr)
+    {
+        if (arr.length <= 1)
+            return arr.length;
         return findMaxSubArray(arr, 0);
     }
 
@@ -120,26 +133,37 @@ public class Ex14 {
      * @param equal   Indicate if first and second are the same.
      * @return Length of flat sequence (counter).
      */
-    public static int lengthFlat(int[] arr, int index, int first, int second, int counter, boolean equal) {
-        if (index == arr.length - 1) return counter;
-        else if (equal) {
-            if (arr[index + 1] == first) {
+    public static int lengthFlat(int[] arr, int index, int first, int second, int counter, boolean equal)
+    {
+        if (index == arr.length - 1)
+            return counter;
+        else if (equal)
+        {
+            if (arr[index + 1] == first)
+            {
                 return lengthFlat(arr, index + 1, first, second, counter + 1, true);
-            } else if (arr[index + 1] + 1 == first || arr[index + 1] - 1 == first) {
+            }
+            else if (arr[index + 1] + 1 == first || arr[index + 1] - 1 == first)
+            {
                 return lengthFlat(arr, index + 1, first, arr[index + 1], counter + 1, false);
-            } else return counter;
-        } else {
-            if (arr[index + 1] == first || arr[index + 1] == second && (first - second == 1 || first - second == -1)) {
+            }
+            else return counter;
+        }
+        else
+        {
+            if (arr[index + 1] == first || arr[index + 1] == second && (first - second == 1 || first - second == -1))
+            {
                 return lengthFlat(arr, index + 1, first, second, counter + 1, false);
             } else return counter;
         }
-
     }
 
-    private static int findMaxSubArray(int[] arr, int index) {
+    private static int findMaxSubArray(int[] arr, int index)
+    {
         if (index == arr.length - 1)
             return 1;
-        else { // Find the maximum length of flat sub-sequence inside array.
+        else
+        { // Find the maximum length of flat sub-sequence inside array.
             return Math.max(lengthFlat(arr, index, arr[index], arr[index + 1], 1, arr[index] == arr[index + 1]), findMaxSubArray(arr, index + 1));
         }
     }
@@ -150,54 +174,47 @@ public class Ex14 {
      * @param mat Input two dimensions array.
      * @return The longest track in array.
      */
-    public static int findMaximum(int[][] mat) {
+    public static int findMaximum(int[][] mat)
+    {
         if (mat[0][0] == -1) return -1;
         return mat[0][0] + findMaximum(mat, 0, 0);
     }
 
-    /**
-     * Calculate longest valid track in 2D array.
-     *
-     * @param mat    Input two dimensions array.
-     * @param row    Row to begin.
-     * @param column Column to begin.
-     * @return The longest track in array.
-     */
-    public static int findMaximum(int[][] mat, int row, int column) {
+    private static int findMaximum(int[][] mat, int row, int column)
+    {
         int down = 0, right = 0, left = 0;
 
-        if (row < mat.length - 1) {
-            if (mat[row + 1][column] == 0) { // going down to 0
-                down = findMaximum(mat, row + 1, column);
-            } else if (mat[row + 1][column] == 1) { // going down to 1
-                down = 1 + findMaximum(mat, row + 1, column);
-            }
+        if (row < mat.length - 1) // Verify array limits
+        {
+            if (mat[row + 1][column] == 0 || mat[row + 1][column] == 1)
+                // going row down to 0 or 1
+                down = mat[row + 1][column] + findMaximum(mat, row + 1, column);
         }
-
-        if (isEqual(row)) {
-            if (column + 1 <= mat[0].length - 1) {
-                if (mat[row][column + 1] == 0) { // going right to 0
-                    right = findMaximum(mat, row, column + 1);
-                } else if (mat[row][column + 1] == 1) { // going right to 1
-                    right = 1 + findMaximum(mat, row, column + 1);
-                }
-            }
+        if (isEval(row)) // row index is eval.
+        {
+            if (column + 1 <= mat[0].length - 1) // Verify array limits
+                if (mat[row][column + 1] == 0 || mat[row][column + 1] == 1)
+                    // going column right to 0 or 1
+                    right = mat[row][column + 1] + findMaximum(mat, row, column + 1);
         }
-        else {
-            if (column - 1 >= 0) {
-                if (mat[row][column - 1] == 0) { // going left to 0
-                    left = findMaximum(mat, row, column - 1);
-                } else if (mat[row][column - 1] == 1) { // going left to 1
-                    left = 1 + findMaximum(mat, row, column - 1);
-                }
-            }
+        else // row index is odd.
+        {
+            if (column - 1 >= 0) // Verify array limits
+                if (mat[row][column - 1] == 0 || mat[row][column - 1] == 1)
+                    // going column left to 0 or 1
+                    left = mat[row][column - 1] + findMaximum(mat, row, column - 1);
         }
-        return (down > left ? down : left) > right ? (down > left ? down : left) : right;
+        return max(max(down,left),right);
     }
 
 
-    private static boolean isEqual(int number) { // returns if number is equal.
+    private static boolean isEval(int number)
+    { // returns if number is eval.
         return number % 2 == 0;
+    }
+    private static int max(int number1, int number2)
+    { // return the bigger number from two input integers.
+        return number1 > number2 ? number1 : number2;
     }
 
 }
